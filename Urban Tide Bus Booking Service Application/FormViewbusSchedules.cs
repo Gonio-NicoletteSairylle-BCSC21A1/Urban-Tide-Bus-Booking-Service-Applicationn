d210d67ac275f138cab1b9cfe8478d1495610edb
Gonio-NicoletteSairylle-BCSC21A1 <nsairyllegonio@gmail.com>
2024-11-01 23:49:44 +0800

    Add project files.

---
+++ Urban Tide Bus Booking Service Application/FormViewbusSchedules.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Urban_Tide_Bus_Booking_Service_Application
+{
+    public partial class FormViewbusSchedules : Form
+    {
+        public FormViewbusSchedules()
+        {
+            InitializeComponent();
+        }
+
+        private void FormViewbusSchedules_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+              
+        }
+    }
+}
